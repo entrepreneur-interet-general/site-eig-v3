1d70006f22c8fb15507cf0c71ad40280d53ba1c8
Lucas Charrier <lucharrier@gmail.com>
2025-02-06 15:21:02 +0100

    chore: add query params filters

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,101 +1,142 @@
 const Isotope = require("isotope-layout");
 
 export default class Grid {
-  constructor() {
-    this.resized = true;
-    this.timeout = null;
-    this.$filters = document.querySelectorAll(".js-filter");
+    constructor() {
+        this.resized = true;
+        this.timeout = null;
+        this.$filters = document.querySelectorAll(".js-filter");
 
-    this.setupIsotope();
-    this.setupToggle();
-  }
+        this.setupIsotope();
+        this.setupToggle();
+        this.applyQueryParams();
+    }
 
-  setupToggle() {
-    document.querySelectorAll(".js-toggle").forEach((button) => {
-      button.addEventListener("click", () => {
-        if (button.classList.contains("fr-mt-4v")) {
-          button.classList.remove("fr-mt-4v");
-          button.textContent = "Afficher plus";
+    setupToggle() {
+        document.querySelectorAll(".js-toggle").forEach((button) => {
+            button.addEventListener("click", () => {
+                if (button.classList.contains("fr-mt-4v")) {
+                    button.classList.remove("fr-mt-4v");
+                    button.textContent = "Afficher plus";
+                } else {
+                    button.classList.add("fr-mt-4v");
+                    button.textContent = "Afficher moins";
+                }
+            });
+        });
+    }
+
+    setupIsotope() {
+        const $isotope = document.querySelector(".js-grid");
+
+        if (!$isotope) return;
+
+        // window.addEventListener('resize', () => this.resized = true);
+        this.setEqualHeight();
+
+        this.isotope = new Isotope($isotope, {
+            layoutMode: "fitRows",
+            getSortData: {
+                name: ".fr-card__link",
+                promo: "[data-promo]",
+            },
+            sortAscending: {
+                promo: false,
+                name: true,
+            },
+            sortBy: ["promo", "name"],
+        });
+
+        // document.querySelector('.js-filter').addEventListener('change', (e) => {
+        //   this.filter(e.target.value);
+        //   this.filter();
+        // });
+
+        this.$filters.forEach((select) => {
+            select.addEventListener("change", () => {
+                this.filter();
+            });
+        });
+    }
+
+    // refresh() {
+    //   console.log('refr');
+    //   if(this.resized) {
+    //     requestAnimationFrame(this.setEqualHeight);
+    //   }
+    //   clearTimeout(this.timeout);
+    //   this.timeout = setTimeout(this.refresh, 100);
+    //   this.resized = false;
+    // }
+
+    setEqualHeight() {
+        const cards = document.querySelectorAll(".js-grid .fr-card");
+        var elems = [].slice.call(cards);
+
+        var tallest = Math.max.apply(
+            Math,
+            elems.map(function (elem, index) {
+                elem.style.minHeight = ""; // clean first
+                return elem.offsetHeight;
+            })
+        );
+
+        elems.forEach(function (elem, index, arr) {
+            elem.style.minHeight = tallest + "px";
+        });
+    }
+
+    updateQueryString(param, value) {
+        const url = new URL(window.location);
+        if (value) {
+            url.searchParams.set(param, value);
         } else {
-          button.classList.add("fr-mt-4v");
-          button.textContent = "Afficher moins";
+            url.searchParams.delete(param);
         }
-      });
-    });
-  }
+        window.history.replaceState({}, "", url);
+    }
 
-  setupIsotope() {
-    const $isotope = document.querySelector(".js-grid");
+    applyQueryParams() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const promoValue = urlParams.get("promos");
+        const expertisesValue = urlParams.get("expertises");
+        const filters = [];
 
-    if (!$isotope) return;
+        if (promoValue) {
+            const promoSelect = this.$filters.find(
+                (select) => select.id === "promos"
+            );
+            if (promoSelect) {
+                promoSelect.value = promoValue;
+                filters.push("." + promoValue);
+            }
+        }
+        if (expertisesValue) {
+            const expertiseSelect = this.$filters.find(
+                (select) => select.id === "expertises"
+            );
+            if (expertiseSelect) {
+                expertiseSelect.value = expertisesValue;
+                filters.push("." + expertisesValue);
+            }
+        }
+        this.isotope.arrange({
+            filter: filters.length > 0 ? filters.join("") : "*",
+        });
+    }
 
-    // window.addEventListener('resize', () => this.resized = true);
-    this.setEqualHeight();
+    filter() {
+        const filters = [];
+        this.$filters.forEach((select) => {
+            if (select.value !== "*") {
+                filters.push("." + select.value);
+                updateQueryString(select.id, select.value);
+            }
+        });
 
-    this.isotope = new Isotope($isotope, {
-      layoutMode: "fitRows",
-      getSortData: {
-        name: ".fr-card__link",
-        promo: "[data-promo]",
-      },
-      sortAscending: {
-        promo: false,
-        name: true,
-      },
-      sortBy: ["promo", "name"],
-    });
-
-    // document.querySelector('.js-filter').addEventListener('change', (e) => {
-    //   this.filter(e.target.value);
-    //   this.filter();
-    // });
-
-    this.$filters.forEach((select) => {
-      select.addEventListener("change", () => {
-        this.filter();
-      });
-    });
-  }
-
-  // refresh() {
-  //   console.log('refr');
-  //   if(this.resized) {
-  //     requestAnimationFrame(this.setEqualHeight);
-  //   }
-  //   clearTimeout(this.timeout);
-  //   this.timeout = setTimeout(this.refresh, 100);
-  //   this.resized = false;
-  // }
-
-  setEqualHeight() {
-    const cards = document.querySelectorAll(".js-grid .fr-card");
-    var elems = [].slice.call(cards);
-
-    var tallest = Math.max.apply(
-      Math,
-      elems.map(function(elem, index) {
-        elem.style.minHeight = ""; // clean first
-        return elem.offsetHeight;
-      })
-    );
-
-    elems.forEach(function(elem, index, arr) {
-      elem.style.minHeight = tallest + "px";
-    });
-  }
-
-  filter() {
-    const filters = [];
-    this.$filters.forEach((select) => {
-      if (select.value !== "*") {
-        filters.push("." + select.value);
-      }
-    });
-
-    this.isotope.arrange({
-      filter: filters.length > 0 ? filters.join("") : "*",
-    });
-  }
+        this.isotope.arrange({
+            filter: filters.length > 0 ? filters.join("") : "*",
+        });
+    }
 }
 
 new Grid();
